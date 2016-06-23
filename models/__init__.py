0288af62606974f12e9ea6d3239efaa5b7e5c3d5
Howard Hamilton <hhamilton@soccermetrics.net>
2016-06-23 03:00:07 -0400

    Import data models so that SQLAlchemy schema can see them

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
 from common import BaseSchema
+
+from enums import (AcquisitionType, ConfederationType, NameOrderType, PositionType)
+from overview import (Clubs, Competitions, CompetitionSeasons, Countries, DomesticCompetitions,
+                      InternationalCompetitions, Persons, Players, Seasons, Years)
+from financial import (AcquisitionPaths, PartialTenures, PlayerDrafts, PlayerSalaries)
+from statistics import (CommonStats, FieldPlayerStats, GoalkeeperStats, LeagueCompetitionPoints)
