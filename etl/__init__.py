456ce72ea8fae882852bccf049e7939ed4d0a760
Howard Hamilton <hhamilton@soccermetrics.net>
2016-07-04 11:13:48 -0400

    Update import list, add CompetitionSeasons and Minutes to CSV_ETL_CLASSES

--- etl/__init__.py
+++ etl/__init__.py
@@ -2,10 +2,11 @@ import os
 import logging
 
 from local import LocalConfig
-from base import BaseCSV, get_local_handles, ingest_feeds, create_seasons
-from overview import (ClubIngest, CountryIngest, CompetitionIngest, PlayerIngest, PersonIngest)
+from base import BaseCSV, SeasonalDataIngest, get_local_handles, ingest_feeds, create_seasons
+from overview import (ClubIngest, CountryIngest, CompetitionIngest, CompetitionSeasonIngest,
+                      PlayerIngest, PersonIngest)
 from financial import (AcquisitionIngest, PlayerSalaryIngest, PartialTenureIngest)
-from statistics import (FieldStatIngest, GoalkeeperStatIngest, LeaguePointIngest)
+from statistics import (PlayerMinuteIngest, FieldStatIngest, GoalkeeperStatIngest, LeaguePointIngest)
 
 
 LOG_FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
@@ -21,10 +22,12 @@ logger.addHandler(ch)
 CSV_ETL_CLASSES = [
     ('Clubs', ClubIngest),
     ('Competitions', CompetitionIngest),
+    ('CompetitionSeasons', CompetitionSeasonIngest),
     ('Players', PlayerIngest),
     ('Acquisitions', AcquisitionIngest),
     ('Salaries', PlayerSalaryIngest),
     ('Partials', PartialTenureIngest),
+    ('Minutes', PlayerMinuteIngest),
     ('FieldStats', FieldStatIngest),
     ('GkStats', GoalkeeperStatIngest),
     ('LeaguePoints', LeaguePointIngest)
