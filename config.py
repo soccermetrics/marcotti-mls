3588c52060b540f6d3ca791c7309b4e9185a60aa
Howard Hamilton <hhamilton@soccermetrics.net>
2016-07-04 00:05:09 -0400

    Replace database_uri method with a property

--- config.py
+++ config.py
@@ -1,18 +1,14 @@
 class Config(object):
     """
-    Base configuration class.  Contains one method that defines the database URI.
+    Base configuration class.  Contains one property that defines the database URI.
 
     This class is to be subclassed and its attributes defined therein.
     """
 
-    def __init__(self):
-        self.database_uri()
-
+    @property
     def database_uri(self):
-        if self.DIALECT == 'sqlite':
-            self.DATABASE_URI = r'sqlite://{name}'.format(name=self.DBNAME)
-        else:
-            self.DATABASE_URI = r'{dialect}://{user}:{passwd}@{host}:{port}/{name}'.format(
+        return r'sqlite://{name}'.format(name=self.DBNAME) if self.DIALECT == 'sqlite' else \
+            r'{dialect}://{user}:{passwd}@{host}:{port}/{name}'.format(
                 dialect=self.DIALECT, user=self.DBUSER, passwd=self.DBPASSWD,
                 host=self.HOSTNAME, port=self.PORT, name=self.DBNAME
             )
