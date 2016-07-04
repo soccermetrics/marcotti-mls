388407eddd6c801272d242b73943280964cca1dc
Howard Hamilton <hhamilton@soccermetrics.net>
2016-07-04 11:03:12 -0400

    Add CompetitionSeasons and Minutes to loader workflow and configuration

--- loader.py
+++ loader.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
                     data_file = os.path.join(*data_file)
                 logger.info("Ingesting %s into %s data model",
                             os.path.join(settings.CSV_DATA_DIR, data_file), entity)
-                if entity in ['Salaries', 'Partials', 'FieldStats', 'GkStats', 'LeaguePoints']:
+                if entity in ['Salaries', 'Partials', 'Minutes', 'LeaguePoints']:
                     params = (sess, settings.COMPETITION_NAME, settings.SEASON_NAME)
                 else:
                     params = (sess,)
